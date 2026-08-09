f41f9fa19fafe930fb9a5de8d5d0b2b0354af813
SGUDestiny <panormov2006@gmail.com>
2026-08-09 18:22:34 +0400

    Some adjustments

--- src/main/java/net/mistersecret312/fairerdeath/Config.java
+++ src/main/java/net/mistersecret312/fairerdeath/Config.java
@@ -20,7 +20,7 @@ public class Config
 	static
 	{
 		KEEPING_MODE = SERVER_BUILDER
-				.comment("Select inventory keeping mode here. It defines the manner in which the items are preserved.")
+				.comment("Select inventory keeping mode here. It defines the manner in which items and experience are preserved.")
 				.comment("Allowed values: FULL, AGE, CHANCE, CATEGORIES, TAG")
 				.comment("FULL - Vanilla keep inventory, full experience and item keeping at all times.")
 				.comment("AGE - Only items that have been in your inventory for X amount of time are kept. X is defined lower at age_ticks.")
@@ -30,14 +30,13 @@ public class Config
 				.comment("Default value: AGE")
 				.defineEnum("keeping_mode", Modes.AGE);
 		CHANCE_TO_KEEP = SERVER_BUILDER
-				.comment("If mode is CHANCE, will use this chance to roll to keep an item.")
+				.comment("If mode is CHANCE, will use this chance to roll to keep an item or half of experience.")
 				.comment("Default value: 0.5")
 				.defineInRange("chance_to_keep", 0.5d, 0d, 1d);
 		AGE_TO_KEEP = SERVER_BUILDER
-				.comment("If mode is AGE, the item will be kept on death if it was for this long in the inventory.")
+				.comment("If mode is AGE, the item will be kept on death if it was for this long in the inventory, same logic for experience.")
 				.comment("Default value: 1200 (60 seconds)")
 				.defineInRange("age_to_keep", 1200, 0, Integer.MAX_VALUE);
-
 		CATEGORIES_TO_KEEP = SERVER_BUILDER
 				.comment("If mode is CATEGORIES, the items in the slot categories selected here will be kept on death.")
 				.comment("Allowed values: KEEP_INVENTORY, KEEP_HOTBAR, KEEP_ARMOR, KEEP_EXPERIENCE")
@@ -45,7 +44,7 @@ public class Config
 				.defineListAllowEmpty(
 						List.of("categories_to_keep"),
 						() -> List.of(Categories.KEEP_HOTBAR.name(), Categories.KEEP_ARMOR.name()),
-									 obj -> obj instanceof String str && Categories.isKnown(str)
+						obj -> obj instanceof String str && Categories.isKnown(str)
 				);
 
 		SERVER_CONFIG = SERVER_BUILDER.build();
